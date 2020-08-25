f4482e9291fdeed650d50b699ae311f8491166e9
emkat93 <eallen5064@students.mchenry.edu>
2020-08-25 11:35:28 -0500

    There was an error

---
+++ src/About Me.kt
@@ -0,0 +1,6 @@
+fun main(){
+    println("Hi! My name is Emily.")
+    println("I want to be a game developer.")
+    println("I have some experience working with the Unreal Engine, 3DS Max, and RPG Maker MV.")
+    println("I hope to continue learning as much as possible so that someday I can develop many games.")
+}
